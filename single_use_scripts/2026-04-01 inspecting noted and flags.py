ded3de22f691b05d64550b45297534b8a25271b9
parke <astroparke@gmail.com>
2026-04-01 15:29:15 -0700

    minor

--- single_use_scripts/2026-04-01 inspecting noted and flags.py
+++ single_use_scripts/2026-04-01 inspecting noted and flags.py
@@ -3,5 +3,6 @@ from stage1_processing import observation_table as obt
 
 #%%
 
+reasons = obt.inspect_values_of_all_tables('reason unusable')
 flags = obt.inspect_values_of_all_tables('flags')
 notes = obt.inspect_values_of_all_tables('notes')
